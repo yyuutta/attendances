e84ffc5453f8a9beaf5fe3dd3d6bf459815c6139
y.yamamoto <y.yamamoto@DESKTOP-S5P1S2V>
2020-02-25 14:28:31 +0900

    全体編集

--- attendance/index.php
+++ attendance/index.php
@@ -4,7 +4,12 @@ ini_set('display_errors', 1);
 
 session_start();
 
-require_once(dirname(__FILE__) . "/../../attendance/library/Dispatcher.php");
+//ローカルじゃなかったら
+if($_SERVER['SERVER_NAME'] != "localhost") {
+    require_once(dirname(__FILE__) . "/../../attendance/library/Dispatcher.php");
+} else {
+    require_once(dirname(__FILE__) . "/../library/Dispatcher.php");
+}
 
 $dispatcher = new Dispatcher();
 $dispatcher->dispatch();
