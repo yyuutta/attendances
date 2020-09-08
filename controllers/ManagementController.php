1d18d003e3681bda425b1237af57a7fae53655be
y.yamamoto <y.yamamoto@DESKTOP-S5P1S2V>
2020-09-08 22:15:28 +0900

    fix

--- controllers/ManagementController.php
+++ controllers/ManagementController.php
@@ -513,7 +513,7 @@ class ManagementController
         $confirm = $action->auth_ch($A, $loginUser_auth);
         
         $comment = $_POST['comment'];
-        $created_at = date("Y/m/d H:i:s");
+        $created_at = to_char(current_timestamp, 'YYYY/MM/DD HH24:MI:SS');
         
         // 既存コメントを格納
         $action = new Management();
